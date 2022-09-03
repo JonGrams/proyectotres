fc272580c18fb22453482ce62eea004547584eea
JonGrams <jonatangrams@gmail.com>
2022-09-03 17:38:06 -0300

    Tabla de multiplicación

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 for (let i = 0; i < 10; i++) {
     alert(i);
 }
+
+// Tabla de multiplicación
+
+let ingresarNumero = Number(prompt("Ingrese un numero"));
+
+for (let i = 1; i <= 10; i++) {
+    let resultado = ingresarNumero * i;
+    alert(ingresarNumero+"x"+i+"="+resultado)
+}
